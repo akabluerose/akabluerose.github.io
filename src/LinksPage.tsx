f78c05f935db8d47e9abb41c7ecda1ee45244614
Sergi Ibanyez <sergi.ibanyez@glovoapp.com>
2023-03-07 22:13:12 +0100

    Adds linter

--- src/LinksPage.tsx
+++ src/LinksPage.tsx
@@ -2,7 +2,6 @@ import './links-page.css'
 import './vars.css'
 import React from "react";
 import {Links} from "./Links";
-import {Link} from "./Link";
 import {Avatar} from "./Avatar";
 
 export const LinksPage = () => {

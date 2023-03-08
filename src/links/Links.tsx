1afd6b68acac11d4e62dd9550fb34f1e19658ede
Sergi Ibanyez <sergi.ibanyez@glovoapp.com>
2023-03-08 12:24:56 +0100

    Adds scrolling on animation end

--- src/links/Links.tsx
+++ src/links/Links.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './links.css'
 import {Link} from "./Link";
 import {animated, config, useSpring} from "@react-spring/web";
 
 export const Links = () => {
+  const [isAnimated, setIsAnimated] = useState(true)
+
   const [linksAnimation, _] = useSpring(() => ({
     from: {opacity: 0, transform: 'translateY(14rem)'},
     to: {opacity: 1, transform: 'translateY(0)'},
     config: config.gentle,
-    delay: 800
+    delay: 800,
+    onRest: () => setIsAnimated(false)
   }), [])
+
+  useEffect(() => {
+    if (!isAnimated) {
+      document.body.classList.remove("prevent-scrolling")
+    }
+  }, [isAnimated])
 
   return <animated.div className={"links-container"} style={linksAnimation}>
     <Link service={"apple"} text={"Apple Music"}/>

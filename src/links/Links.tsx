ccc6b1068d442c3949f25b94e2407de24b1c8417
Sergi Ibanyez <sergi.ibanyez@glovoapp.com>
2023-03-08 11:06:37 +0100

    Adds animation on load

--- src/links/Links.tsx
+++ src/links/Links.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import './links.css'
 import {Link} from "./Link";
+import {animated, config, useSpring} from "@react-spring/web";
 
 export const Links = () => {
-  return <div className={"links-container"}>
+  const [linksAnimation, _] = useSpring(() => ({
+    from: {opacity: 0, transform: 'translateY(14rem)'},
+    to: {opacity: 1, transform: 'translateY(0)'},
+    config: config.gentle,
+    delay: 800
+  }), [])
+
+  return <animated.div className={"links-container"} style={linksAnimation}>
     <Link service={"apple"} text={"Apple Music"}/>
     <Link service={"spotify"} text={"Spotify"}/>
     <Link service={"youtube"} text={"Youtube"}/>
     <Link service={"latest"} text={"\"No Molestar\" ft. La Maldad"}/>
-  </div>
+  </animated.div>
 }
--- src/links/avatar/Avatar.tsx
+++ src/links/avatar/Avatar.tsx
@@ -1,11 +1,20 @@
+import {animated, config, easings, useSpring} from '@react-spring/web'
 import AvatarIcon from '../../assets/avatar2.png'
 import './avatar.scss'
+import React from "react";
 
 export const Avatar = () => {
+  const [avatarAnimation, _] = useSpring(() => ({
+    from: {opacity: 0, transform: 'translateY(-7rem)'},
+    to: {opacity: 1, transform: 'translateY(0)'},
+    config: config.gentle,
+    delay: 400
+  }), [])
+
   return (
-      <div className={"avatar-container"}>
+      <animated.div className={"avatar-container"} style={avatarAnimation}>
         <img alt="avatar" src={AvatarIcon}/>
         <span>AKA BLUEROSE</span>
-      </div>
+      </animated.div>
   )
 }
